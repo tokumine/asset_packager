b3719cd382d7f884f10a94a9578d3f6c30ef675e
Simon Tokumine <simon@japancentre.com>
2009-11-08 03:43:56 +0000

    updated to catch failures due to too many API calls

--- lib/synthesis/asset_package.rb
+++ lib/synthesis/asset_package.rb
@@ -163,7 +163,9 @@ module Synthesis
         output = []
         @sources.each {|s|
           options[:js_code] = File.open("#{@asset_path}/#{s}.#{@extension}", "r") { |f| f.read }
-          output << Net::HTTP.post_form(URI.parse(api_url), options).body
+          compiled_js = Net::HTTP.post_form(URI.parse(api_url), options).body
+          retry if compiled_js == "Error(22): Too many compiles performed recently.  Try again later."
+          output << compiled_js
         }
         output.join
       end
